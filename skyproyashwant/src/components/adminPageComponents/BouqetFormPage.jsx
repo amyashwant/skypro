536fa72d2f45c2427f171fbe85106ff497852510
vpdevabacus <developer@abacusmail.in>
2023-12-11 12:35:47 +0530

    hello

--- skyproyashwant/src/components/adminPageComponents/BouqetFormPage.jsx
+++ skyproyashwant/src/components/adminPageComponents/BouqetFormPage.jsx
@@ -1,7 +1,62 @@
 import React, { useState } from 'react';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import ListItemText from '@mui/material/ListItemText';
+import Select from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
 import PortalHeader from './adminHeader.jsx/PortalHeader';
 
+const channels = [
+    'star plus',
+    'mtv',
+    'aat tak'
+];
+
+const broadcasters = [
+    'Discovery Communications India',
+    'Celebrities Management Pvt Ltd',
+    "Eenadu Television Pvt Ltd"
+];
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
 const BouqetFormPage = () => {
+
+
+   const [broadcastername, setBroadcasterName] = useState([]);
+   const [channelName, setchannelName] = useState([]);
+
+  const handleBroadcasterChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setBroadcasterName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+  const handleChannelChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setchannelName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
   const [formData, setFormData] = useState({
     name: '',
     price: '',
@@ -33,30 +88,51 @@ const BouqetFormPage = () => {
         />
       </div>
 
-      <div className="mb-3">
-        <label className="form-label">Broadcaster Name:</label>
-        <input
-          type="text"
-          className="form-control"
-          name="price"
-          value={formData.price}
-        />
-      </div>
-      
-
-      <div className="mb-3">
-        <label className="form-label checkbox">Channel:</label>
-        <select
-          className="form-select"
-          name="channels"
-          value={formData.channels}
-        //   onChange={handleInputChange}
-        //   multiple 
+      <div>
+      <FormControl sx={{ m: 1, width: 600 }}>
+        <InputLabel id="demo-multiple-checkbox-label">Broadcaster</InputLabel>
+        <Select
+          labelId="demo-multiple-checkbox-label"
+          id="demo-multiple-checkbox"
+          multiple
+          value={broadcastername}
+          onChange={handleBroadcasterChange}
+          input={<OutlinedInput label="Broadcaster" />}
+          renderValue={(selected) => selected.join(', ')}
+          MenuProps={MenuProps}
         >
-          <option value="Channel1">Channel</option>
-          <option value="Channel1">Channel</option>
-        </select>
-      </div>
+          {broadcasters.map((broadcaster) => (
+            <MenuItem key={broadcaster} value={broadcaster}>
+              <Checkbox checked={broadcastername.indexOf(broadcaster) > -1} />
+              <ListItemText primary={broadcaster} />
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+      
+    <div>
+    <FormControl sx={{ m: 1, width: 600 }}>
+        <InputLabel id="demo-multiple-checkbox-label">Channel Type</InputLabel>
+        <Select
+          labelId="demo-multiple-checkbox-label"
+          id="demo-multiple-checkbox"
+          multiple
+          value={channelName}
+          onChange={handleChannelChange}
+          input={<OutlinedInput label="Channel Type" />}
+          renderValue={(selected) => selected.join(', ')}
+          MenuProps={MenuProps}
+        >
+          {channels.map((channel) => (
+            <MenuItem key={channel} value={channel}>
+              <Checkbox checked={channelName.indexOf(channel) > -1} />
+              <ListItemText primary={channel} />
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
 
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
--- skyproyashwant/src/components/adminPageComponents/ChannelFormHeader.jsx
+++ skyproyashwant/src/components/adminPageComponents/ChannelFormHeader.jsx
@@ -1,7 +1,61 @@
 import React, { useState } from "react";
 import PortalHeader from "./adminHeader.jsx/PortalHeader";
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import ListItemText from '@mui/material/ListItemText';
+import Select from '@mui/material/Select';
+import Checkbox from '@mui/material/Checkbox';
+
+const channels = [
+  'star plus',
+  'mtv',
+  'aat tak'
+];
+const languages = [
+  'Hindi',
+  'English',
+  'Punjabi'
+];
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 
 const ChannelFormPage = () => {
+
+  const [channelName, setchannelName] = useState([]);
+  const [languageName, setlanguageName] = useState([]);
+
+  const handleChannelChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setchannelName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+  const handleLanguageChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setlanguageName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+
+
   const [formData, setFormData] = useState({
     name: "",
     price: "",
@@ -33,25 +87,51 @@ const ChannelFormPage = () => {
           />
         </div>
 
-        <div className="mb-3">
-          <label className="form-label">Channel Type:</label>
-          <input
-            type="text"
-            className="form-control"
-            name="price"
-            value={formData.type}
-          />
-        </div>
+        <div>
+      <FormControl sx={{ m: 1, width: 600 }}>
+        <InputLabel id="demo-multiple-checkbox-label">Channel Type</InputLabel>
+        <Select
+          labelId="demo-multiple-checkbox-label"
+          id="demo-multiple-checkbox"
+          multiple
+          value={channelName}
+          onChange={handleChannelChange}
+          input={<OutlinedInput label="Channel Type" />}
+          renderValue={(selected) => selected.join(', ')}
+          MenuProps={MenuProps}
+        >
+          {channels.map((channel) => (
+            <MenuItem key={channel} value={channel}>
+              <Checkbox checked={channelName.indexOf(channel) > -1} />
+              <ListItemText primary={channel} />
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
 
-        <div className="mb-3">
-        <label className="form-label">Language:</label>
-        <input
-          type="text"
-          className="form-control"
-          name="lang"
-          value={formData.lang}
-        />
-      </div>
+    <div>
+      <FormControl sx={{ m: 1, width: 600 }}>
+        <InputLabel id="demo-multiple-checkbox-label">Language</InputLabel>
+        <Select
+          labelId="demo-multiple-checkbox-label"
+          id="demo-multiple-checkbox"
+          multiple
+          value={languageName}
+          onChange={handleLanguageChange}
+          input={<OutlinedInput label="Language" />}
+          renderValue={(selected) => selected.join(', ')}
+          MenuProps={MenuProps}
+        >
+          {languages.map((language) => (
+            <MenuItem key={language} value={language}>
+              <Checkbox checked={languageName.indexOf(language) > -1} />
+              <ListItemText primary={language} />
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
 
         <div className="mb-3">
           <label className="form-label">Channel Image:</label>
