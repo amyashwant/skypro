0f08e76cf0c7f6d53c10c2cb0bd517767124203b
amyashwant <yashwantmeerasingh2002@gmail.com>
2023-12-19 15:36:06 +0530

    new changes

--- skyproyashwant/src/components/homePageComponents/testimmonials/Testimonials.jsx
+++ skyproyashwant/src/components/homePageComponents/testimmonials/Testimonials.jsx
@@ -20,7 +20,7 @@ const Testimonials = () => {
     autoplay: true,
     pauseOnHover: false,
     easing: "linear",
-    fade: true,
+    fade: false,
     arrows: true,
   };
 
@@ -32,20 +32,20 @@ return (
             <div className="section-header">
               <h4 className="subtitle text-white">Client Testimonials</h4>
             </div>
-            <div className="testimonial-left py-100">
+            <div className="testimonial-left position-relative py-100">
               <div className="row align-items-center">
                 <div className="col-sm-12 text-center">
                   <Slider {...settings} className="">
                     {testimonialData.map((item, i) => (
                       <div className="testi-content" key={i}>
                         <div className="icon mb-lg-4">
-                          <i className={item?.icon}></i>
+                          <img src={item?.image} alt="img" />
+                        <div className="meta-thumb">
+                          {console.log("iamge 1", item.image)}
                         </div>
-                        <p className="para text-white">{item.description}</p>
+                        </div>
+                        <p className="para text-white"><i class="fa-solid fa-quote-left"></i>{item.description}<i class="fa-solid fa-quote-right"></i></p>
                         <div className="client-meta d-flex align-items-center flex-wrap mt-5">
-                          <div className="meta-thumb">
-                            <img src={item.image} alt="img" />
-                          </div>
                           <div className="meta-content">
                             <h5 className="name">{item.name}</h5>
                             <span className="designation">{item.designation}</span>
@@ -77,14 +77,14 @@ const testimonialData = [
   {
     icon: "icon-quote",
     description: "I have changed my perception about the services that I was experiencing in the past during 2001-2009. It is so comforting to see your engineer, Mangesh Ghatge, drop in at our place within half an hour of our registering a complaint.",
-    image: testThree,
+    image: testTwo,
     name: "Wade Warren",
     designation: "CEO"
   },
   {
     icon: "icon-quote",
     description: "One of the two YOU Broadband connections wasn't working in our office for some time. It is so comforting to see your engineer, Mangesh Ghatge, drop in at our place within half an hour of our registering a complaint.",
-    image: testFour,
+    image: testTwo ,
     name: "Abby Covert",
     designation: "CO-founder"
   },
