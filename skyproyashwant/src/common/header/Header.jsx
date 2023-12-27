e1b0fff47fc76ee4f0921392b6360be57ae6e267
amyashwant <poonamgangwar083@gmail.com>
2023-12-27 16:03:11 +0530

    twoth

--- skyproyashwant/src/common/header/Header.jsx
+++ skyproyashwant/src/common/header/Header.jsx
@@ -1,183 +1,3 @@
-// import React, { useContext, useState } from "react";
-// import bottomImg from "../../assets/images/home-01/logo/Skypro_New_Logo.png";
-// import { Link, useNavigate } from "react-router-dom";
-
-// import AccountContext from "../../utils/AccountContext";
-// const Header = () => {
-//   const { login, setLogin, setUserData } = useContext(AccountContext);
-//   const navigate = useNavigate();
-//   const logoutHandler = (e) => {
-//     e.preventDefault();
-//     localStorage.removeItem("userInfo");
-//     setLogin(false);
-//     setUserData(null);
-//     navigate("/login");
-//   };
-
-//   return (
-//     <>
-//       <div className="header-top d-none d-lg-block">
-//         <div className="container">
-//           <div className="top-header-wrapper d-flex flex-wrap justify-content-between align-items-center">
-//             <div className="top-contact">
-//               <ul className="top-contact-menu d-flex flex-wrap justify-content-between align-items-center">
-//                 <li className="item">
-//                   <span className="icon-pin"></span>
-//                   <p>Abacus Cloud, B-70 ,Sector-74, Phase-7 Mohali, Punjab</p>
-//                 </li>
-//                 {/* <li className="item">|</li>
-//                 <li className="item">
-//                   <span className="icon-mail"></span>
-//                   <a href="mailto:">info123@gmail.com</a>
-//                 </li> */}
-//               </ul>
-//             </div>
-//             <div className="top-button d-flex flex-wrap justify-content-between align-items-center">
-//               <ul className="login-registration d-flex flex-wrap justify-content-between align-items-center">
-//                 {localStorage.getItem("userInfo") ? (
-//                   <li>
-//                     <Link to="/">
-//                       <span className="icon-user-1"></span>
-//                       {/* <span onClick={logoutHandler}>LogOut</span> */}
-//                       <span onClick={logoutHandler}>Admin</span>
-//                     </Link>
-//                   </li>
-//                 ) : (
-//                   <>
-//                     <span className="social-icons d-flex align-items-center flex-wrap header-social-icon">
-//                       <li>
-//                         <Link
-//                           to="https://www.facebook.com/tvskypro/"
-//                           target="_blank"
-//                         >
-//                           <i className="fab fa-facebook-f"></i>
-//                         </Link>
-//                       </li>
-//                       <li>
-//                         <Link
-//                           to="https://twitter.com/Skypro_TV"
-//                           target="_blank"
-//                         >
-//                           <i className="fab fa-twitter"></i>
-//                         </Link>
-//                       </li>
-//                       <li>
-//                         <Link
-//                           to="https://www.linkedin.com/company/skyprotv/"
-//                           target="_blank"
-//                         >
-//                           <i className="fab fa-linkedin-in"></i>
-//                         </Link>
-//                       </li>
-//                       <li>
-//                         <Link
-//                           to="https://in.pinterest.com/skyprotv/"
-//                           target="_blank"
-//                         >
-//                           <i className="fab fa-pinterest-p"></i>
-//                         </Link>
-//                       </li>
-//                     </span>
-//                     <li>
-//                       <Link to="/login">
-//                         <span className="icon-user-1"></span>
-//                         Admin
-//                       </Link>
-//                     </li>
-//                     {/* <li>/</li> */}
-//                     <li>{/* <Link to="/register">Register</Link> */}</li>
-//                   </>
-//                 )}
-//               </ul>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-
-//       <header className="header-bottom home-two-header">
-//         <div className="container">
-//           <nav className="navbar navbar-expand-lg navbar-light">
-//             <Link className="navbar-brand logo" to="/">
-//               <img src={bottomImg} alt="img" />
-//             </Link>
-
-//             <button
-//               className="navbar-toggler header-button"
-//               type="button"
-//               data-bs-toggle="collapse"
-//               data-bs-target="#navbarSupportedContent"
-//               aria-controls="navbarSupportedContent"
-//               aria-expanded="false"
-//               aria-label="Toggle navigation"
-//             >
-//               <i className="fa-solid fa-bars"></i>
-//             </button>
-
-//             <div
-//               className="collapse navbar-collapse"
-//               id="navbarSupportedContent"
-//             >
-//               <ul className="navbar-nav nav-menu ms-auto">
-//                 <li className="nav-item d-block d-lg-none"></li>
-//                 <li className="nav-item dropdown">
-//                   <Link
-//                     className="nav-link active"
-//                     to="/"
-//                     role="button"
-//                     aria-expanded="false"
-//                   >
-//                     HOME
-//                   </Link>
-//                 </li>
-//                 <li className="nav-item dropdown">
-//                   <Link
-//                     className="nav-link"
-//                     to="/about"
-//                     role="button"
-//                     aria-expanded="false"
-//                   >
-//                     ABOUT
-//                   </Link>
-//                 </li>
-//                 {/* <li className="nav-item dropdown">
-//                   <Link
-//                     className="nav-link"
-//                     to="/service"
-//                     role="button"
-//                     aria-expanded="false"
-//                   >
-//                     SERVICES
-//                   </Link>
-//                 </li> */}
-//                 <li className="nav-item dropdown">
-//                   <Link
-//                     className="nav-link"
-//                     to="/packages"
-//                     role="button"
-//                     aria-expanded="false"
-//                   >
-//                     PACKAGES
-//                   </Link>
-//                 </li>
-
-//                 <li className="nav-item">
-//                   <Link className="nav-link" to="/contact">
-//                     CONTACT US
-//                   </Link>
-//                 </li>
-//               </ul>
-//             </div>
-//           </nav>
-//         </div>
-//       </header>
-//     </>
-//   );
-// };
-
-// export default Header;
-
-
-
 import React, { useContext, useEffect, useRef, useState } from "react";
 import bottomImg from "../../assets/images/home-01/logo/Skypro_New_Logo.png";
 import { Link, useLocation, useNavigate } from "react-router-dom";
@@ -200,52 +20,8 @@ const Header = () => {
   const location = useLocation();
 
   const isActive = (path) => {
-    return location.pathname === path ? 'active' : '';
+    return location.pathname === path ? "active" : "";
   };
-
-  // const handleScroll = (elTopOffset, elHeight) => {
-  //   if (window.pageYOffset > (elTopOffset + elHeight)) {
-  //     setSticky({ isSticky: true, offset: elHeight });
-  //   } else {
-  //     setSticky({ isSticky: false, offset: 0 });
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   var header = headerRef.current.getBoundingClientRect();
-  //   const handleScrollEvent = () => {
-  //     handleScroll(header.top, header.height)
-  //   }
-  //   window.addEventListener('scroll', handleScrollEvent);
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScrollEvent);
-  //   };
-  // }, []);
-
-  // const handleScroll = () => {
-  //   const scrollPosition = window.scrollY;
-  //   const headerHeight = headerRef.current.offsetHeight;
-  
-  //   if (scrollPosition > headerHeight) {
-  //     setSticky({ isSticky: true, offset: headerHeight });
-  //   } else {
-  //     setSticky({ isSticky: false, offset: 0 });
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   const handleScrollEvent = () => {
-  //     handleScroll();
-  //   };
-  
-  //   window.addEventListener('scroll', handleScrollEvent);
-  
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScrollEvent);
-  //   };
-  // }, []);
-  
-  
   return (
     <>
       <div className={`header-top d-none d-lg-block`}>
@@ -270,7 +46,7 @@ const Header = () => {
                   <li>
                     <Link to="/">
                       <span className="icon-user-1"></span>
-                    <span onClick={logoutHandler}>LogOut</span> 
+                      <span onClick={logoutHandler}>LogOut</span>
                       <span onClick={logoutHandler}>Admin</span>
                     </Link>
                   </li>
@@ -310,14 +86,6 @@ const Header = () => {
                         </Link>
                       </li>
                     </span>
-                    {/* <li>
-                      <Link to="/login">
-                        <span className="icon-user-1"></span>
-                        Admin
-                      </Link>
-                    </li> */}
-                    {/* {/ <li>/</li> /} */}
-                    {/* <li><Link to="/register">Register</Link> </li> */}
                   </>
                 )}
               </ul>
@@ -326,60 +94,78 @@ const Header = () => {
         </div>
       </div>
 
-      {/* <header className={`header-bottom home-two-header ${sticky.isSticky ? 'sticky' : ''}`} ref={headerRef}> */}
-      <header className='header-bottom home-two-header'>
-      {/* {/ <header className={`header-bottom home-two-header ${scrolling? "visible": ""}`}> /} */}
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light">
-          <Link className="navbar-brand logo" to="/">
-            <img src={bottomImg} alt="img" />
-          </Link>
+      <header className="header-bottom home-two-header">
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light">
+            <Link className="navbar-brand logo" to="/">
+              <img src={bottomImg} alt="img" />
+            </Link>
 
-          <button
-            className="navbar-toggler header-button"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <i className="fa-solid fa-bars"></i>
-          </button>
+            <button
+              className="navbar-toggler header-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <i className="fa-solid fa-bars"></i>
+            </button>
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav nav-menu ms-auto">
-              <li className="nav-item d-block d-lg-none"></li>
-              <li className="nav-item dropdown">
-                <Link className={`nav-link ${isActive('/')}`} to="/" role="button" aria-expanded="false">
-                  HOME
-                </Link>
-              </li>
-              <li className="nav-item dropdown">
-                <Link className={`nav-link ${isActive('/about')}`} to="/about" role="button" aria-expanded="false">
-                  ABOUT
-                </Link>
-              </li>
-              {/* {/ Add similar logic for other navigation items /} */}
-              <li className="nav-item dropdown">
-                <Link className={`nav-link ${isActive('/packages')}`} to="/packages" role="button" aria-expanded="false">
-                  PACKAGES
-                </Link>
-              </li>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav nav-menu ms-auto">
+                <li className="nav-item d-block d-lg-none"></li>
+                <li className="nav-item dropdown">
+                  <Link
+                    className={`nav-link ${isActive("/")}`}
+                    to="/"
+                    role="button"
+                    aria-expanded="false"
+                  >
+                    HOME
+                  </Link>
+                </li>
+                <li className="nav-item dropdown">
+                  <Link
+                    className={`nav-link ${isActive("/about")}`}
+                    to="/about"
+                    role="button"
+                    aria-expanded="false"
+                  >
+                    ABOUT
+                  </Link>
+                </li>
+                {/* {/ Add similar logic for other navigation items /} */}
+                <li className="nav-item dropdown">
+                  <Link
+                    className={`nav-link ${isActive("/packages")}`}
+                    to="/packages"
+                    role="button"
+                    aria-expanded="false"
+                  >
+                    PACKAGES
+                  </Link>
+                </li>
 
-              <li className="nav-item">
-                <Link className={`nav-link ${isActive('/contact')}`} to="/contact">
-                  CONTACT US
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </header>
+                <li className="nav-item">
+                  <Link
+                    className={`nav-link ${isActive("/contact")}`}
+                    to="/contact"
+                  >
+                    CONTACT US
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </header>
     </>
   );
 };
 
 export default Header;
-
